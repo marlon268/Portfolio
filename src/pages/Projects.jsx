c1ab223b0b2995bed7229f50169843b2ff1345ad
marlon268 <varon.marlon83@gmail.com>
2021-06-30 10:32:56 -0500

    primer prototipo terminado

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,5 +1,26 @@
 import React from "react";
+import "../style/components/project.css";
+import logoProject from "../images/projet_amarillo.svg";
+import RickAndMorty from "../images/RickAndMorty.PNG";
 
 export function Projects() {
-  return <main className="projects"></main>;
+  return (
+    <main className="projects">
+      <div className="projects-logo">
+        <img src={logoProject} alt="" />
+        <h1>Projects</h1>
+      </div>
+      <section className="projects-container">
+        <div className="projects-project">
+          <a href="https://marlon268.github.io/RickAndMorty/" target="_blank">
+            <p>Visitar en la web</p>
+          </a>
+          <img src={RickAndMorty} alt="" />
+          <a href="https://github.com/marlon268/RickAndMorty" target="_blank">
+            <p>Repositorio en GitHub</p>
+          </a>
+        </div>
+      </section>
+    </main>
+  );
 }
